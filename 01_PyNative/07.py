a1f07ae43ea791242deee3e9b22bb3277f383ff7
viter-line <lv.bodnar.andrew@gmail.com>
2025-11-25 17:51:56 +0200

    +++

---
+++ 01_PyNative/07.py
@@ -0,0 +1,6 @@
+# # Exercise 7: Find the number of occurrences of a substring in a string
+# # Write a Python code to find how often the substring “Emma” appears in the given string.
+#
+# Given:
+str_x = "Emma is good developer. Emma is a writer"
+print("Emma appeared",str_x.count("Emma"), "times")
