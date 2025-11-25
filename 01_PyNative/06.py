02aec8a347ac5d629ac0c8201554636f68049a84
viter-line <lv.bodnar.andrew@gmail.com>
2025-11-25 15:43:15 +0200

    today

--- 01_PyNative/06.py
+++ 01_PyNative/06.py
@@ -1,10 +1,7 @@
 # Exercise 6: Display numbers divisible by 5
 # Write a Python code to display numbers from a list divisible by 5
 
-a = input()
-a = list(a)
-print(type(a))
-print(a)
-for i in a:
-    if (i % 5 == 0):
-        print("Displaying numbers divisible by 5:")
+a = [1, 3, 5, 10,23, 15, 20, 43, 50, 60]
+for x in a:
+    if x % 5 == 0:
+        print("Displaying numbers divisible by 5:",x)
