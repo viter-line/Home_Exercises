24990beefd7c1f2c7f8538237f798fe72154d0f8
viter-line <lv.bodnar.andrew@gmail.com>
2025-12-08 11:11:52 +0200

    review

--- 01_PyNative/01_Basic_Exercise_for_Beginners/21.py
+++ 01_PyNative/01_Basic_Exercise_for_Beginners/21.py
@@ -1,3 +1,12 @@
+# Exercise 21: Check if a user-entered string contains any digits using a for loop
+# Expected Output:
+
+# Enter a string: Pynative123Python
+# The string contains at least one digit.
+
+# Enter a string: PYnative
+# The string does not contain any digits.
+
 sentensec = input("Enter a sentence: ")
 
 for i in sentensec:
---
+++ 01_PyNative/01_Basic_Exercise_for_Beginners/22.py
@@ -0,0 +1,9 @@
+# Exercise 22: Capitalize the first letter of each word in a string
+# Expected Output:
+
+# str1 = "pynative.com is for python lovers"
+# # Output Pynative.com Is For Python Lovers
+
+str1 = "pynative.com is for python lovers"
+str2 = str1.split()
+print(str2.upper())
