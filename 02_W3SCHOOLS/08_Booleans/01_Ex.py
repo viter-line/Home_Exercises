1affb00bbab4144134ff6abef0e8f66b8aaae86d
viter-line <lv.bodnar.andrew@gmail.com>
2025-12-19 21:24:45 +0200

    19 December

---
+++ 02_W3SCHOOLS/08_Booleans/01_Ex.py
@@ -0,0 +1,29 @@
+# Завдання 1. Логічні значення
+
+# 🔹 Умова:
+
+# Створи дві змінні:
+
+# is_python_fun зі значенням True
+
+# is_snow_hot зі значенням False
+
+# Виведи:
+
+# значення кожної змінної
+
+# її тип
+
+# 📌 Вимоги:
+
+# Значення повинні бути саме True / False, не рядки
+
+# Для типу використовуй type()
+
+is_python_fub = True
+
+is_show_hot = False
+
+print(is_python_fub, type(is_python_fub))
+
+print(is_show_hot, type(is_show_hot))
---
+++ 02_W3SCHOOLS/09_Operators/03_Ex.py
@@ -0,0 +1,31 @@
+# авдання 3. Логічні оператори
+
+# 🔹 Умова:
+
+# Створи дві логічні змінні:
+
+# has_ticket
+
+# has_id
+
+# Обчисли та виведи:
+
+# has_ticket and has_id
+
+# has_ticket or has_id
+
+# not has_ticket
+
+# 📌 Вимоги:
+
+# Значення змінних задай вручну
+
+# Без використання if
+
+has_ticket = True
+
+has_id = False
+
+print("has_ticket and has_id:",has_ticket and has_id)
+print("has_ticket or has_id:", has_ticket or has_id)
+print("not has_ticket:", not has_ticket)
