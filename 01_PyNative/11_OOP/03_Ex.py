9f02065ffad04603ffb153fd8dde99dd1989be26
viter-line <lv.bodnar.andrew@gmail.com>
2025-12-20 23:20:59 +0200

    20 December

---
+++ 01_PyNative/11_OOP/03_Ex.py
@@ -0,0 +1,13 @@
+#Create a Vehicle class without any variables and methods
+# OOP Exercise 3: Create a child class Bus that will inherit all of the variables and methods of the Vehicle class
+
+
+class Vehicle:
+    def __init__(self, name, max_speed, mileage):
+        self.name = name
+        self.max_speed = max_speed
+        self.mileage = mileage
+
+VolVoX = Vehicle(XVers, 310, 200000)
+
+print(VolVoX.name, VolVoX.max_speed, VolVoX.mileage)
